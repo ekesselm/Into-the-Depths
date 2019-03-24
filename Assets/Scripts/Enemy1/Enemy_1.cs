26ad2ea33a38ef7d44ad17a15b39f19a21716d99
Emilio Kessel <ekesselm@gmail.com>
2019-03-24 23:55:41 +0100

    #66 Programación e implementación enemigo 1

---
+++ Assets/Scripts/Enemy1/Enemy_1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1 : MonoBehaviour
+{
+    private bool movingRight = true;
+
+    public Transform Limit;
+
+    public float speed;
+
+    void Update()
+    {
+        
+        // Moverse de un lado a otro
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(Limit.position, Vector2.down, 2f);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+       }
+
+
+
+}
+
+  
+
---
+++ Assets/Scripts/Enemy1/Enemy_1_attack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1_attack : MonoBehaviour
+{
+    public int tiempoAtaque = 1;
+
+    Animator Enemy1Animator;
+
+    public Rigidbody2D Player;
+
+    public bool atacando;
+
+    float speed;
+
+    public GameObject PlayerPos;
+
+    public Vector3 fuerzaEmpujon;
+
+    public int distanciaEnemigo = 4;
+
+    void Start(){
+
+        Enemy1Animator = gameObject.GetComponent<Animator>();
+        atacando = false;
+    }
+
+    IEnumerator tiempoEsperaAtaque(){
+        speed = 0;
+        yield return new WaitForSeconds(tiempoAtaque);
+        Enemy1Animator.SetBool("detenerse", true);
+        Enemy1Animator.SetBool("atacar", false);
+        atacando = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        Debug.Log("enemy detected");
+        atacando = true;
+        Enemy1Animator.SetBool("atacar", true);
+        StartCoroutine("tiempoEsperaAtaque");
+        Player.AddForce(fuerzaEmpujon, ForceMode2D.Impulse);
+    }
+}
