67bc1b9c3bb428cb8eabcbd468045bf6f4f30a7b
ekesselm <47969897+ekesselm@users.noreply.github.com>
2019-03-26 09:24:46 +0100

    #66 Programación del enemigo 1

---
+++ Assets/Scripts/Enemy1/EnemyGroundAttack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyGroundMovement))]
+public class EnemyGroundAttack : MonoBehaviour
+{
+
+    public float tiempoAtaque = 0.60f;
+    
+    public float fuerzaEmpujon;
+
+    private GameObject player;
+    private Animator Enemy1Animator;
+    
+    public void Empujon()
+    {
+        bool movingRight = GetComponent<SpriteRenderer>().flipX;
+        Vector2 direction = Vector2.right;
+        if (!movingRight) direction = Vector2.left;
+        player.GetComponent<Rigidbody2D>().AddForce(direction * fuerzaEmpujon, ForceMode2D.Impulse);
+    }
+
+    void Start()
+    {
+        Enemy1Animator = gameObject.GetComponent<Animator>();
+
+    }
+
+    IEnumerator tiempoEsperaAtaque()
+    {
+        yield return new WaitForSeconds(tiempoAtaque);
+        GetComponent<EnemyGroundMovement>().isAttacking = false;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+    
+        if (col.transform.CompareTag("Player")){
+                Debug.Log("enemy detected");
+                Enemy1Animator.SetBool("atacar", true);
+                GetComponent<EnemyGroundMovement>().isAttacking = true;
+                player = col.gameObject;
+                StartCoroutine("tiempoEsperaAtaque");            
+            }
+        }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Enemy1Animator.SetBool("atacar", false);
+    }
+
+}
+
+
+
+  
+
---
+++ Assets/Scripts/Enemy1/EnemyGroundMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyGroundAttack))]
+public class EnemyGroundMovement : MonoBehaviour
+{
+
+    public Transform Limit;
+    public float speed;
+
+    public bool isAttacking = false;
+    public bool isStopped = false;
+
+    private bool movingRight = true;
+
+    void Update()
+    {
+        if (!isAttacking)
+            Movement();
+    } 
+
+    private void Movement()
+    {
+
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
+        if (!Physics2D.Raycast(Limit.position, Vector2.down, 2f).collider)
+        {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+
+            movingRight = !movingRight;
+        }
+    }
+
+}
+
+  
+
