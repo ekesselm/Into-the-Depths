235dc7ef0cfa4520654a17e788f7e52b601579df
Emilio K <ekesselm@gmail.com>
2019-04-25 13:34:18 +0200

    Environment design

---
+++ Assets/Scripts/Limita.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limita : MonoBehaviour
+{
+    public Animator player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.transform.tag == "Player")
+        {
+            player.SetBool("andar", false);
+        }
+    }
+}
