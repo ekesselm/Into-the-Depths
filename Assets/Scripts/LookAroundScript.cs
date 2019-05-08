fdad263e98ce897093c165a54fd0dcdbdc357975
Emilio K <ekesselm@gmail.com>
2019-05-08 19:25:44 +0200

    Dungeon environment #1 WIP

---
+++ Assets/Scripts/LookAroundScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAroundScript : MonoBehaviour
+{
+    public GameObject LookAroundObject;
+    public float speed = 10f;
+    private Vector2 positionTarget;
+    public Camera camera;
+
+    private void Start()
+    {
+        Physics.queriesHitTriggers = true;
+        positionTarget = LookAroundObject.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+    }
+
+    private void OnMouseOver()
+    {
+        positionTarget = camera.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Nueva posición " + positionTarget);
+        LookAroundObject.transform.position = Vector2.MoveTowards(LookAroundObject.transform.position, positionTarget, Time.deltaTime * speed);
+        Debug.Log("LookAroundObject " + LookAroundObject.transform.position);
+    }
+    
+
+
+}
