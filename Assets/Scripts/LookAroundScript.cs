4156e0d1fd9e22fe834086e24350ef75d0df97b6
Emilio K <ekesselm@gmail.com>
2019-05-09 14:18:10 +0200

    Look Around #1

--- Assets/Scripts/LookAroundScript.cs
+++ Assets/Scripts/LookAroundScript.cs
@@ -11,22 +11,32 @@ public class LookAroundScript : MonoBehaviour
 
     private void Start()
     {
+        // Necesario para usar el OnMouseOver en un trigger
         Physics.queriesHitTriggers = true;
+
+        // Target inicial de la cámara: La posición por defecto del personaje
         positionTarget = LookAroundObject.transform.position;
     }
 
     private void Update()
     {
-        transform.position = GameObject.FindGameObjectWithTag("Player").transform.position;
-
+        // El objeto vacío sigue al jugador (ya que no puede ser hijo de este)
+        LookAroundObject.transform.position = Vector2.MoveTowards(LookAroundObject.transform.position, positionTarget, Time.deltaTime * speed);
+            ;
     }
 
     private void OnMouseOver()
     {
-        positionTarget = camera.ScreenToWorldPoint(Input.mousePosition);
+        // Nueva posición objetivo
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, 1.0f);
+        positionTarget = camera.ScreenToWorldPoint(mousePosition);
         Debug.Log("Nueva posición " + positionTarget);
-        LookAroundObject.transform.position = Vector2.MoveTowards(LookAroundObject.transform.position, positionTarget, Time.deltaTime * speed);
-        Debug.Log("LookAroundObject " + LookAroundObject.transform.position);
+
+
+        // Mientras el ratón esé en el area, el objeto vacío se mueve hacia la pos del ratón
+        //Debug.Log("LookAroundObject " + LookAroundObject.transform.position);
     }
     
 
