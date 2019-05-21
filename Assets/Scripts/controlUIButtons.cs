85fa7bae86bcc918f39f140efdb81a145fb8a94a
Emilio K <ekesselm@gmail.com>
2019-05-22 00:19:37 +0200

    Main menu added

---
+++ Assets/Scripts/controlUIButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlUIButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NuevaPartida()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Opciones()
+    {
+
+    }
+
+    public void CerrarPartida()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
