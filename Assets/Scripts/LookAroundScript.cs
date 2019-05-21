cea98d4b7b725a27c3475f6d8f13acb632f3c742
Emilio K <ekesselm@gmail.com>
2019-05-21 10:33:07 +0200

    Trying to repair look around

--- Assets/Scripts/LookAroundScript.cs
+++ Assets/Scripts/LookAroundScript.cs
@@ -7,32 +7,28 @@ public class LookAroundScript : MonoBehaviour
     public GameObject LookAroundObject;
     public float speed = 10f;
     private Vector2 positionTarget;
-    public Camera camera;
+    public Camera gameCamera;
 
     private void Start()
     {
         // Necesario para usar el OnMouseOver en un trigger
         Physics.queriesHitTriggers = true;
-
-        // Target inicial de la cámara: La posición por defecto del personaje
-        positionTarget = LookAroundObject.transform.position;
     }
 
     private void Update()
     {
-        // El objeto vacío sigue al jugador (ya que no puede ser hijo de este)
-        LookAroundObject.transform.position = Vector2.MoveTowards(LookAroundObject.transform.position, positionTarget, Time.deltaTime * speed);
-            ;
     }
 
     private void OnMouseOver()
     {
         // Nueva posición objetivo
 
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = new Vector3(mousePosition.x, mousePosition.y, 1.0f);
-        positionTarget = camera.ScreenToWorldPoint(mousePosition);
-        Debug.Log("Nueva posición " + positionTarget);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = new Vector2(mousePosition.x, mousePosition.y);
+        LookAroundObject.transform.position = mousePosition;
+
+        Debug.Log("Input.mousePosition " + Input.mousePosition);
+        Debug.Log("Nueva posición " + mousePosition);
 
 
         // Mientras el ratón esé en el area, el objeto vacío se mueve hacia la pos del ratón
